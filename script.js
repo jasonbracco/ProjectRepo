d58b77187692b5c381a27113ec628da1c92e0282
Jason Bracco <bracco.jason@gmail.com>
2022-04-16 09:49:29 -0400

    addingin bio paragraphs and headers

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector('body').appendChild(h2)
+const h3 = document.createElement("h2");
+h2.textContent = "About me: Jason Bracco";
+document.querySelector('body').appendChild(h3)
