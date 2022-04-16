0e33b1ef926218dc786138488ee39edacb2c31c8
Jason Bracco <bracco.jason@gmail.com>
2022-04-16 10:52:11 -0400

    Finishing touches with event listener

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-const h3 = document.createElement("h2");
-h2.textContent = "About me: Jason Bracco";
+const h3 = document.createElement("h3");
+h3.textContent = "Welcome To My World";
 document.querySelector('body').appendChild(h3)
+
+const picture = document.getElementById('jasonGreece')
+jasonGreece.addEventListener('click', function(){
+    alert('YO SICK FLICK RIGHT?');
+})
